1b4a740e17b6d7eb32cb467ced9a4201f9b9c764
ElliotFrias <elliotetian@gmail.com>
2023-11-23 17:22:05 -0600

    Jala por favor

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ dotenv.config()
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  cors: {
+    origin: 'https://chathosteadoback-production.up.railway.app/',
+    methods: ['GET', 'POST'],
+  },
+})
 
 const PORT = process.env.PORT ?? 3000
 
