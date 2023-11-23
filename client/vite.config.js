458433ccb52779ee5812a9fcc73639e6a53c836c
ElliotFrias <elliotetian@gmail.com>
2023-11-23 15:57:28 -0600

    Espero que si sea la buena

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const TARGET = process.env.ORIGIN
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/socket.io': {
-        target: 'http://localhost:3000',
+        target: TARGET,
         ws: true,
       },
     },
